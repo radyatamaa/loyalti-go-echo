89f106d6b1fda0e30731fd9fe7b9ee72ee35ec7e
felixsiburian <felixsiburian10@gmail.com>
2019-12-06 16:02:40 +0700

    Add Employee Repository

--- src/domain/repository/outlet_repository.go
+++ src/domain/repository/outlet_repository.go
@@ -31,7 +31,7 @@ func DeleteOutlet(outlet *model.Outlet) string {
 }
 
 
-func GetOutlet(page *int, size *int, sort *int) []model.Outlet {
+func GetOutlet(page *int, size *int, sort *int, id *int) []model.Outlet {
 	db := database.ConnectionDB()
 	//db := database.ConnectPostgre()
 	var outlet []model.Outlet
