3dafee04a1636ff671eff4235ca6503d56f9cab3
felixsiburian <felixsiburian10@gmail.com>
2019-12-09 19:02:12 +0700

    Add Program Point

--- src/database/migration/main.go
+++ src/database/migration/main.go
@@ -1,40 +1,16 @@
 package main
 
 import (
+	"fmt"
 	_ "github.com/jinzhu/gorm/dialects/mssql"
 	"github.com/radyatamaa/loyalti-go-echo/src/database"
 	"github.com/radyatamaa/loyalti-go-echo/src/domain/model"
-	"time"
 )
 
 func main() {
 
 	db := database.ConnectionDB()
-	program := model.Program{
-		Created:            time.Now(),
-		CreatedBy:          "Admin",
-		Modified:           time.Now(),
-		ModifiedBy:         "Admin",
-		Active:             true,
-		IsDeleted:          false,
-		Deleted:            nil,
-		Deleted_by:         "",
-		ProgramName:        "Diskon 70% Goride",
-		ProgramImage:       "",
-		ProgramStartDate:   time.Now(),
-		ProgramEndDate:     time.Now(),
-		ProgramDescription: "Diskon Goride sampai dengan 70% maksimal 10k",
-		Card:               "Member",
-		OutletID:           "1",
-		MerchantId:         9,
-		CategoryId:         2,
-		Benefit:            "Tidak ada",
-		TermsAndCondition:  "Tidak ada",
-		Tier:               "Platinum",
-		RedeemRules:        "Tidak ada",
-		RewardTarget:       0,
-		QRCodeId:           "1",
-	}
-	db.Create(&program)
+	db.AutoMigrate(model.Program{})
+	fmt.Println("migrasi berhasil")
 	
 }
