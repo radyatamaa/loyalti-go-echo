406bb3e8d93264e021b50dd8920d680c8f8e05af
davidnobels <davidnobelsht@gmail.com>
2019-12-12 13:39:56 +0700

    bug fix

--- src/database/migration/main.go
+++ src/database/migration/main.go
@@ -1,22 +1,14 @@
 package main
 
 import (
-	"github.com/radyatamaa/loyalti-go-echo/src/domain/model"
 	_ "github.com/jinzhu/gorm/dialects/mssql"
-	"github.com/radyatamaa/loyalti-go-echo/src/database"
-	"fmt"
 )
 
 func main() {
-	//var id = 16
-	//var pay = 7000
-	//repository.TotalPoint(&id, &pay)
+
 
 	//guids := guid.New()
 	//fmt.Println(guids)
 
-	db := database.ConnectionDB()
-	db.AutoMigrate(&model.ProductCategory{})
-	fmt.Println("migrasi berhasil")
 }
 
