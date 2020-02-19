d2de7aec045119d548bd8bc1d173bbc86233dc53
felixsiburian <felixsiburian10@gmail.com>
2020-02-19 11:53:56 +0700

    Unit Test Merchant

--- src/database/migration/main.go
+++ src/database/migration/main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	_ "github.com/jinzhu/gorm/dialects/mssql"
-	"github.com/radyatamaa/loyalti-go-echo/src/database"
-	"github.com/radyatamaa/loyalti-go-echo/src/domain/model"
-
-	//"github.com/radyatamaa/loyalti-go-echo/src/database"
-
+//	"github.com/radyatamaa/loyalti-go-echo/src/database"
+//"github.com/radyatamaa/loyalti-go-echo/src/domain/model"
 )
 
 func main() {
-	db := database.ConnectionDB()
-	db.AutoMigrate(&model.Card{})
+
 	}
 
 
